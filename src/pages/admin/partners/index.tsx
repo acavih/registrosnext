229b22a355899202dab4c149b9e7e7104bc306ca
registros <89086611+acavih@users.noreply.github.com>
2023-09-29 20:45:24 +0000

    Hacer layout y página de login

---
+++ src/pages/admin/partners/index.tsx
@@ -0,0 +1,9 @@
+import { AuthedComponent } from "~/components/AuthedComponent"
+
+export default AuthedComponent(function PartnersPage () {
+    return (
+        <p>
+            Pagina de socios
+        </p>
+    )
+})
